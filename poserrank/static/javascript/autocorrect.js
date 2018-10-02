da7d6156f67435c3b9b32b719b0c2cc43ff1aeac
Alex Shadley <shadleyalex@gmail.com>
2018-10-01 21:15:59 -0500

    adds autocorrect functionality

---
+++ poserrank/static/javascript/autocorrect.js
@@ -0,0 +1,41 @@
+"use strict";
+
+function Autobox(elem, apiSource) {
+    this.elem = elem;
+    this.apiSource = apiSource;
+
+    this.datalist = document.getElementById(this.elem.getAttribute('list'));
+
+    this.updateDropdown = function() {
+        this.queryApi().then((response) => this.updateHTML(response))
+    };
+
+    this.updateHTML = function(suggestions) {
+        // remove all existing nodes
+        while(this.datalist.firstChild) {
+            this.datalist.removeChild(this.datalist.firstChild);
+        }
+        for(let i = 0; i < suggestions.length; i++) {
+            let opt = document.createElement('OPTION');
+            opt.value = suggestions[i];
+            this.datalist.appendChild(opt);
+        }
+    };
+
+    this.queryApi = function() {
+        let form = new FormData();
+        form.append('query', elem.value);
+        return fetch(this.apiSource, {method: 'POST', body: form})
+            .then((response) => response.json())
+    };
+}
+
+window.addEventListener('load', function() {
+    let autoboxes = document.getElementsByClassName('autocomplete');
+    for(let i = 0; i < autoboxes.length; i ++) {
+        let abox = new Autobox(autoboxes[i], window.location.origin + '/api/users/search');
+        autoboxes[i].addEventListener('input', function(event) {
+            abox.updateDropdown();
+        });
+    }
+});
